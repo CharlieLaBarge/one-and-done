adee8399ad53175efc7d8cbd3e603152fe0f994d
Charlie Barge <artemisc360@gmail.com>
2016-11-12 19:32:31 -0500

    provisioning

--- hello.py
+++ hello.py
@@ -2,12 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+from datetime import datetime
 
 # route goes here
 @app.route("/")
 def hello():
-    return "Hello world!"
+    now = datetime.utcnow()
+    then = datetime(1970, 1, 1)
+    return ("Number of seconds since 1970: " + str((now - then).total_seconds()))
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
--- provision.sh
+++ provision.sh
@@ -6,6 +6,37 @@ set -x
 
 echo "provisioning!"
 
+sudo apt-get update -y
+sudo apt-get install -y python-pip python-dev nginx
+
+sudo pip install virtualenv
+
+mkdir /home/vagrant/myproject
+cd /home/vagrant/myproject
+
+virtualenv myprojectenv
+
+source myprojectenv/bin/activate
+
+pip install gunicorn flask
+
+cp /vagrant/hello.py /home/vagrant/myproject/myproject.py
+cp /vagrant/wsgi.py /home/vagrant/myproject/wsgi.py
+
+deactivate
+
+sudo cp /vagrant/myproject.service /etc/systemd/system/
+sudo systemctl start myproject
+sudo systemctl enable myproject
+
+sudo cp /vagrant/myproject /etc/nginx/sites-available/myproject
+sudo rm -rf /etc/nginx/sites-enabled/default
+
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo systemctl restart nginx
+
+sudo ufw allow 'Nginx Full'
+
 # TODO your code here
 
 echo "provisioning complete!"
